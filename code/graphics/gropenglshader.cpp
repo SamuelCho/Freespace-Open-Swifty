c71e79058bd8f8792013cf177cfa5b91d708fd07
Samuel Cho <samuel.s.cho@gmail.com>
2015-04-11 00:25:02 -0700

    Only append #version 120 to shaders on Mac.

--- code/graphics/gropenglshader.cpp
+++ code/graphics/gropenglshader.cpp
@@ -309,7 +309,9 @@ static char *opengl_load_shader(shader_type type_id, char *filename, int flags)
 {
 	SCP_string sflags;
 
+#ifdef __APPLE__
     sflags += "#version 120\n";
+#endif
     
 	if (Use_GLSL >= 4) {
 		sflags += "#define SHADER_MODEL 4\n";
